adfbbd86b971b479094dc000da2f9a11976667cb
vatch123 <vatsalyachaubey19980@gmail.com>
2021-07-08 23:34:00 +0530

    bonus

---
+++ Week2/bonus.sql
@@ -0,0 +1,10 @@
+-- Insert a new pizza to the pizza database
+INSERT INTO pizza_names
+  ("pizza_id", "pizza_name")
+VALUES
+  (3, 'Supreme');
+
+INSERT INTO pizza_recipes
+  ("pizza_id", "toppings")
+VALUES
+  (3, '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12');
